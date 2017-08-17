1fcbcdba9bb38798bae1cdf5cd26189d0bef9e7a
roctbb <roctbb@gmail.com>
2017-08-17 15:31:54 +0300

    words

---
+++ Day 1 - Euler/6- Words.py
@@ -0,0 +1,13 @@
+word = input('Введите слово:').lower()
+
+old_words = []
+
+while True:
+    new_word = input('Введите слово:').lower()
+    if word[-1] == new_word[0] and new_word not in old_words:
+        print('Подходит')
+        old_words.append(new_word)
+        word = new_word
+        print('Теперь другой игрок!')
+    else:
+        print('Не подходит')
