d0189402b6c71cef8353fe6de0b7f84d0814036f
roctbb <roctbb@gmail.com>
2017-08-22 13:16:00 +0300

    tornadogram + requests

---
+++ Day 8 - Tornado/extras - requests api.py
@@ -0,0 +1,7 @@
+import requests
+
+response = requests.get("http://127.0.0.1:8888/notify?token=123&type=add&comment=blabla&amount=300")
+
+answer = response.json()
+print(answer)
+print(answer["result"])
--- Day 8 - Tornado/extras - tornadogram.py
+++ Day 8 - Tornado/extras - tornadogram.py
@@ -9,13 +9,27 @@ bot = telebot.TeleBot(token=token)
 
 user = -1
 
-@bot.message_handler(content_types=["text"])
+# telegram
+
+@bot.message_handler(commands=["auth"])
+def auth_handler(message):
+    message = bot.send_message(message.chat.id, "Введите токен:")
+    bot.register_next_step_handler(message, make_auth)
+
+@bot.message_handler(commands=["subscribe"])
 def repeat_all_messages(message):
     print("Got message")
     global user
     user = message.chat.id
     bot.send_message(user, "Вы подписались")
 
+
+
+def make_auth(message):
+    text = message.text
+    bot.send_message(message.chat.id, "Ваш токен: "+text)
+
+#server
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -25,11 +39,13 @@ class MainHandler(tornado.web.RequestHandler):
         comment = self.get_argument("comment")
         amount = self.get_argument("amount")
 
-        self.write("Отправляю уведомление токену {0}, тип - {1},"
-                   "количество - {2}, коммент - {3}".format(token,type,comment,amount))
         print(user)
         if user != -1:
             bot.send_message(user, "Уведомление")
+            self.write('{"result": "OK"}')
+        else:
+            self.write('{"result": "NOT FOUND"}')
+
 
 def start_server():
     routes = [
