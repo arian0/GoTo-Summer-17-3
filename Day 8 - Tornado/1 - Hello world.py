9daf0bc822b6da67ee44db8940c52b8febe641d7
roctbb <roctbb@gmail.com>
2017-08-24 12:45:47 +0300

    start web

--- Day 8 - Tornado/1 - Hello world.py
+++ Day 8 - Tornado/1 - Hello world.py
@@ -5,12 +5,14 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("<h2>Hello, world</h2>")
         self.write("<img width='400px' src='https://icdn.lenta.ru/images/2016/08/04/12/20160804125254027/pic_d86815bab0fb639499d43ca75ed4760f.jpg'>")
+        self.write("<form action='/hello'><label>Как вас зовут?</label><input name='name' /><button>Отправить</button></form>")
 
 # localhost:8888/hello?name=Rostislav
 class HelloHandler(tornado.web.RequestHandler):
     def get(self):
         name = self.get_argument("name", "аноним")
-        self.write("Привет, {0}!".format(name))
+        self.render("hello_template.html", name=name)
+
 
 routes = [
     (r"/", MainHandler),
