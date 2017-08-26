0b4df8bf6b3ccae990a638e0f15b5c9ce9f9168b
roctbb <roctbb@gmail.com>
2017-08-26 10:17:49 +0300

    mongodb task

--- Day 8 - Tornado/Imageboard/server.py
+++ Day 8 - Tornado/Imageboard/server.py
@@ -1,12 +1,15 @@
 import tornado.ioloop
 import tornado.web
 import json
+from bson import ObjectId
+
+from pymongo import MongoClient
 
 '''
 1. главная страница - список тредов - MainHandler - GET - /
 2. добавление треда - MainHandler - POST - /
-3. список постов в треде - ThreadHandler - GET - /thread/?id=THREAD_ID
-4. добавление поста в тред - ThreadHandler - POST - /thread/?id=THREAD_ID
+3. список постов в треде - ThreadHandler - GET - /thread?id=THREAD_ID
+4. добавление поста в тред - ThreadHandler - POST - /thread?id=THREAD_ID
 
 [
     {"name": "мемы", "posts": [
@@ -20,12 +23,19 @@ import json
 ]
 '''
 
+# создать конекшн к монге
+connection = MongoClient("mongodb://user:password@ds159963.mlab.com:59963/goto_imageboard")
+
+# выбрать бд
+database = connection['goto_imageboard']
+
+# выбрать коллекцию
+threads_collection = database['threads']
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         # выдача страницы с тредами
-        threads = []
-        with open('data.json') as file:
-            threads = json.loads(file.read())
+        threads = list(threads_collection.find())
 
         self.render("threads.html", threads=threads)
 
@@ -34,13 +44,7 @@ class MainHandler(tornado.web.RequestHandler):
         name = self.get_argument("name")
         thread = {"name": name, "posts": []}
 
-        threads = []
-        with open('data.json') as file:
-            threads = json.loads(file.read())
-        threads.append(thread)
-
-        with open('data.json', 'w') as file:
-            file.write(json.dumps(threads))
+        threads_collection.insert(thread)
 
         self.redirect('/')
 
@@ -49,16 +53,23 @@ class MainHandler(tornado.web.RequestHandler):
 class ThreadHandler(tornado.web.RequestHandler):
     def get(self):
         # выдача страницы с постами
+        # TODO: Написать шаблон для страницы с постами (пожий на главную страницу)
+        # TODO: Взять тред из монги, у которого {'_id': ObjectId(self.get_argument('id'))}
+        # TODO: Зарендерить шаблон с постами этого треда
         pass
 
     def post(self):
         # добавление поста
+        # TODO: Получить параметры из запроса (self.get_argument...)
+        # TODO: сделать новый пост
+        # TODO: добавить пост в тред и сохранить в монге
+        # TODO: редирект на страницу с постами
         pass
 
 
 routes = [
     (r"/", MainHandler),
-    (r"/thread/", ThreadHandler),
+    (r"/thread", ThreadHandler),
 ]
 
 app = tornado.web.Application(routes, debug=True)
